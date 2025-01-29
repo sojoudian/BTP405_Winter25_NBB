1b6393d60634361aa497a3054b7dfca58c264201
maziar <sojodyan@gmail.com>
2025-01-29 16:41:25 -0500

    add w4 l1

--- Week4/lab01.py
+++ Week4/lab01.py
@@ -1,3 +1,11 @@
 import http.server
 import socketserver
 
+PORT = 8000
+
+
+handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print(f"Server is running at http://localhost:{PORT}")
+    httpd.serve_forever()
+

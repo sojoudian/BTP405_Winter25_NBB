400352dd5a21c6d71950e4a4d07e2e22b25255bd
maziar <sojodyan@gmail.com>
2025-02-12 17:14:49 -0500

    add post_clinet.py

---
+++ Week06/post_client.py
@@ -0,0 +1,26 @@
+import http.client
+import json
+import time
+
+url = 'localhost:8081'
+
+# Sample data to send (change eeach time)
+payload = {"timestamp": time.time(), "message": "Hello from Client!"}
+json_payload = json.dumps(payload)
+
+# Create connection to the server
+conn = http.client.HTTPConnection(url)
+
+# Send POST reuest
+headers = {'Content-type', 'application/json'}
+conn.request("POST", "/", body=json_payload, headers=headers)
+
+# Get response
+response = conn.getresponse()
+response_data = response.read().decode("utf-8")
+
+# Log response to the consle
+print("Response: ", response.status, response_data)
+
+# Close conenction
+conn.close()
